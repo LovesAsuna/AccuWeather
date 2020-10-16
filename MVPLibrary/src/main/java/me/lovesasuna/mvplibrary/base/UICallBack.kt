106732c6d5747424e6f60e8f42e1fdd50ae26f3c
LovesAsuna <qq625924077@gmail.com>
2020-10-16 23:00:19 +0800

    refactor(interface): change type
    更改类型以允许空类型

--- MVPLibrary/src/main/java/me/lovesasuna/mvplibrary/base/UICallBack.kt
+++ MVPLibrary/src/main/java/me/lovesasuna/mvplibrary/base/UICallBack.kt
@@ -3,12 +3,12 @@ package me.lovesasuna.mvplibrary.base
 import android.os.Bundle
 
 interface UICallBack {
-    //初始化savedInstanceState
-    fun initBeforeView(savedInstanceState: Bundle)
+    // 初始化savedInstanceState
+    fun initBeforeView(savedInstanceState: Bundle?)
 
-    //初始化
-    fun initData(savedInstanceState: Bundle)
+    // 初始化
+    fun initData(savedInstanceState: Bundle?)
 
-    //布局
+    // 布局
     fun getLayoutId(): Int
 }
