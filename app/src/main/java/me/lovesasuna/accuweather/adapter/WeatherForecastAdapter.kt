4fa3d4d4d06f1e279125773d06a0cb4ce39fe016
LovesAsuna <qq625924077@gmail.com>
2020-10-16 23:10:42 +0800

    feat(adapter): 天气预报适配器

---
+++ app/src/main/java/me/lovesasuna/accuweather/adapter/WeatherForecastAdapter.kt
@@ -0,0 +1,26 @@
+package me.lovesasuna.accuweather.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import me.lovesasuna.accuweather.R
+import me.lovesasuna.accuweather.bean.DailyForecastsItem
+
+/**
+ * @author LovesAsuna
+ * @date 2020/10/16 22:26
+ *
+ *
+ * 天气预报列表展示适配器
+ */
+class WeatherForecastAdapter(
+    layoutResId: Int,
+    data: List<DailyForecastsItem>
+) : BaseQuickAdapter<DailyForecastsItem, BaseViewHolder>(layoutResId, data) {
+
+    override fun convert(helper: BaseViewHolder, item: DailyForecastsItem) {
+        helper.setText(R.id.tv_date, item.date) //日期
+            .setText(R.id.tv_info, item.day?.iconPhrase) //天气
+            .setText(R.id.tv_low_and_height, "${item.temperature?.minimum}/${item.temperature?.maximum}℃") //最低温和最高温
+    }
+
+}
