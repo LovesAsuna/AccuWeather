6ffd5c26bb0ae0e8389fa6e5495f4281e75dfd59
LovesAsuna <qq625924077@gmail.com>
2020-10-17 13:06:06 +0800

    refactor(contract): abstract function
    将获取locationkey的代码抽象为一个函数

--- app/src/main/java/me/lovesasuna/accuweather/contract/WeatherContract.kt
+++ app/src/main/java/me/lovesasuna/accuweather/contract/WeatherContract.kt
@@ -3,6 +3,7 @@ package me.lovesasuna.accuweather.contract
 import me.lovesasuna.accuweather.MainActivity
 import me.lovesasuna.accuweather.api.APIService
 import me.lovesasuna.accuweather.bean.ForecastResponse
+import me.lovesasuna.accuweather.bean.LifeStyleResponse
 import me.lovesasuna.accuweather.bean.LocationResponse
 import me.lovesasuna.accuweather.bean.WeatherResponse
 import me.lovesasuna.mvplibrary.base.BasePresenter
@@ -16,12 +17,15 @@ import retrofit2.Response
 /**
  * @author LovesAsuna
  * @date 2020/10/14 23:43
- **/
-/**
+ *
+ *
  * 天气订阅器
  */
 class WeatherContract {
     class WeatherPresenter : BasePresenter<IWeatherView>() {
+
+        val service = createService(APIService::class.java)
+
         /**
          * 当日天气
          * @param context
@@ -29,36 +33,26 @@ class WeatherContract {
          */
         fun todayWeather(context: MainActivity.MyLocationListener, location: String) {
 
-            //得到构建之后的网络请求服务，这里的地址已经拼接完成，只差一个location了
-            val service = createService(APIService::class.java)
-
-            service.getLocation(location).enqueue(object : NetCallBack<LocationResponse>() {
-                override fun onSuccess(call: Call<LocationResponse>, response: Response<LocationResponse>) {
-                    //设置请求回调  NetCallBack是重写请求回调
-                    service.getTodayWeather(getView()?.getCurrentLocationResult(response)!!).enqueue(object :
-                        NetCallBack<WeatherResponse>() {
-                        //成功回调
-                        override fun onSuccess(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
-                            if (getView() != null) {
-                                //当视图不会空时返回请求数据
-                                getView()?.getTodayWeatherResult(response)
-                            }
+            actionByLocation(service, location) {
+                //设置请求回调  NetCallBack是重写请求回调
+                service.getTodayWeather(getView()?.getCurrentLocationResult(it)!!).enqueue(object :
+                    NetCallBack<WeatherResponse>() {
+                    //成功回调
+                    override fun onSuccess(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                        if (getView() != null) {
+                            //当视图不会空时返回请求数据
+                            getView()?.getTodayWeatherResult(response)
                         }
+                    }
 
-                        //失败回调
-                        override fun onFailed() {
-                            if (getView() != null) { //当视图不会空时获取错误信息
-                                getView()?.dataFailed()
-                            }
+                    //失败回调
+                    override fun onFailed() {
+                        if (getView() != null) { //当视图不会空时获取错误信息
+                            getView()?.dataFailed()
                         }
-                    })
-                }
-
-                override fun onFailed() {
-                    getView()?.dataFailed()
-                }
-
-            })
+                    }
+                })
+            }
 
         }
 
@@ -69,28 +63,62 @@ class WeatherContract {
          * @param location
          */
         fun weatherForecast(context: MainActivity.MyLocationListener, location: String) {
-            val service = createService(APIService::class.java)
 
-            service.getLocation(location).enqueue(object : NetCallBack<LocationResponse>() {
-                override fun onSuccess(call: Call<LocationResponse>, response: Response<LocationResponse>) {
-                    //设置请求回调  NetCallBack是重写请求回调
-                    service.getWeatherForecast(getView()?.getCurrentLocationResult(response)!!).enqueue(object :
-                        NetCallBack<ForecastResponse>() {
-                        //成功回调
-                        override fun onSuccess(call: Call<ForecastResponse>, response: Response<ForecastResponse>) {
+            actionByLocation(service, location) {
+                //设置请求回调  NetCallBack是重写请求回调
+                service.getWeatherForecast(getView()?.getCurrentLocationResult(it)!!).enqueue(object :
+                    NetCallBack<ForecastResponse>() {
+                    //成功回调
+                    override fun onSuccess(call: Call<ForecastResponse>, response: Response<ForecastResponse>) {
+                        if (getView() != null) {
+                            //当视图不会空时返回请求数据
+                            getView()?.getWeatherForecastResult(response)
+                        }
+                    }
+
+                    //失败回调
+                    override fun onFailed() {
+                        if (getView() != null) { //当视图不会空时获取错误信息
+                            getView()?.dataFailed()
+                        }
+                    }
+                })
+            }
+        }
+
+        /**
+         * 生活指数
+         * @param context
+         * @param location
+         */
+        fun lifeStyle(context: MainActivity.MyLocationListener, location: String) {
+
+            actionByLocation(service, location) {
+                service.getDailyIndex(getView()?.getCurrentLocationResult(it)!!)
+                    .enqueue(object : NetCallBack<LifeStyleResponse>() {
+                        override fun onSuccess(call: Call<LifeStyleResponse>, response: Response<LifeStyleResponse>) {
                             if (getView() != null) {
-                                //当视图不会空时返回请求数据
-                                getView()?.getWeatherForecastResult(response)
+                                getView()!!.getLifeStyleResult(response)
                             }
                         }
 
-                        //失败回调
                         override fun onFailed() {
-                            if (getView() != null) { //当视图不会空时获取错误信息
+                            if (getView() != null) {
                                 getView()?.dataFailed()
                             }
                         }
                     })
+            }
+        }
+
+        private fun actionByLocation(
+            service: APIService,
+            location: String,
+            action: (response: Response<LocationResponse>) -> Unit
+        ) {
+            service.getLocation(location).enqueue(object : NetCallBack<LocationResponse>() {
+                override fun onSuccess(call: Call<LocationResponse>, response: Response<LocationResponse>) {
+                    action.invoke(response)
                 }
 
                 override fun onFailed() {
@@ -98,6 +126,7 @@ class WeatherContract {
                 }
 
             })
+
         }
 
     }
@@ -112,6 +141,9 @@ class WeatherContract {
 
         //查询天气预报的数据返回
         fun getWeatherForecastResult(response: Response<ForecastResponse>)
+
+        //查询生活指数的数据返回
+        fun getLifeStyleResult(response: Response<LifeStyleResponse>)
 
         //错误返回
         fun dataFailed()
